ab81c2171f2b46ac48876148d56aa5ca3ff03f2a
tajgr <levjakub@gmail.com>
2014-09-16 17:05:20 +0200

    Add zbar_test.py

---
+++ zbar_test.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+"""
+  The tool for barcode reading
+"""
+
+import io
+import time
+import picamera
+from PIL import Image
+import zbar
+import sys
+
+def runBarcodeReader():
+#   Create the in-memory stream
+    stream = io.BytesIO()
+    camera = picamera.PiCamera()
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture(stream, format='jpeg')
+#   "Rewind" the stream to the beginning so we can read its content
+    stream.seek(0)
+    pil = Image.open(stream)
+#
+#########################################
+#
+#   create a reader
+    scanner = zbar.ImageScanner()
+
+#   configure the reader
+    scanner.parse_config('enable')
+    
+    pil = pil.convert('L')
+    width, height = pil.size
+    raw = pil.tostring()
+    
+#   wrap image data
+    image = zbar.Image(width, height, 'Y800', raw)
+    
+#   scan the image for barcodes
+    scanner.scan(image)
+
+#   extract results
+    for symbol in image:
+#       print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+        print symbol.type, symbol.data
+    
+#   clean up
+    del(image)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print __doc__
+        sys.exit()
+    runBarcodeReader()
