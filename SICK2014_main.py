f1dd1664cf934ae878338cb1980c7fe18b048b32
tajgr <levjakub@gmail.com>
2014-09-24 19:02:31 +0200

    test images

--- SICK2014_main.py
+++ SICK2014_main.py
@@ -7,13 +7,18 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
-# test function
-def sickMain():
+from picamera_tool import imageSave
+
+def ledLight():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(11, GPIO.OUT)
     GPIO.output(11, True)
     time.sleep(5)
     GPIO.output(11, False)
+
+def sickMain():
+#    pass
+    imageSave( 30.0 )
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
