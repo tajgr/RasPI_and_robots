904494dc7b7f9b8bd94eaff9c45a14a790f9d567
tajgr <levjakub@gmail.com>
2014-10-07 19:55:59 +0200

    number detection

--- SICK2014_main.py
+++ SICK2014_main.py
@@ -4,10 +4,12 @@
 """
 
 import RPi.GPIO as GPIO
+import picamera
 import time
 import sys
 
-from picamera_tool import imageSave
+from picamera_tool import *
+from Digit_detect import *
 
 def ledLight():
     GPIO.setmode(GPIO.BOARD)
@@ -16,12 +18,48 @@ def ledLight():
     time.sleep(5)
     GPIO.output(11, False)
 
-def sickMain():
-#    pass
-    imageSave( 30.0 )
+def sickMain( workingTime = 60, saving = True ):
+    detected = []
+    logs = open("/home/pi/git/RasPI_and_robots/logs/detection.txt", "a")
+    stream = io.BytesIO()
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+#    camera.start_preview()
+    time.sleep(2)
+    A=Digit_detect('',20)
+    A.learn_from_file('/home/pi/git/RasPI_and_robots/samples.data','/home/pi/git/RasPI_and_robots/responses.data')
+    index = 0
+    timeStart = time.time()
+    actulalTime = time.time()
+    while timeStart > actulalTime - workingTime:
+        actulalTime = time.time()
+        
+        camera.capture(stream, format='jpeg')
+        #    Construct a numpy array from the stream
+        data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+        #    "Decode" the image from the array, preserving colour
+        image = cv2.imdecode(data, 1)
+        stream.seek(0)
+        stream.truncate()
+        
+        if saving:
+#            actualTime = time.time()
+            filename = timeName( "/home/pi/git/RasPI_and_robots/logs/image_", ".jpg", str(index) )
+#            camera.capture( filename )
+            cv2.imwrite( filename, image )
+            index = index + 1
+#        print A.detect_digits_from_file('SICK_ROBOT/001.jpg')
+        print filename
+#        detected = A.detect_digits_from_file(filename)
+        detected = A.detect_digits(image)
+        print detected
+        logs.write(filename + str(detected ) + "\r\n")
+        logs.flush()
+    
+    logs.close()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print __doc__
         sys.exit()
-    sickMain()
+    sickMain( workingTime = 10 )
