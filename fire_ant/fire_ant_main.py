ae1870fab1adff5d30092e17579ec22ac0e7c8e5
tajgr <levjakub@gmail.com>
2015-06-01 12:00:28 +0200

    Final version for RR

--- fire_ant/fire_ant_main.py
+++ fire_ant/fire_ant_main.py
@@ -7,6 +7,21 @@ import RPi.GPIO as GPIO
 import picamera
 import time
 import sys
+import datetime
+import serial
+import math
+import io
+from thread import start_new_thread
+
+sys.path.append( "/home/pi/git/fireant/serial_servo" )
+sys.path.append( "/home/pi/git/fireant/ver1")
+sys.path.append( "/home/pi/git/fireant/ver0")
+from serial_servo import LogIt, ReplayLog, ReplyLogInputsOnly, LogEnd
+from fireant import *
+from triangle import pos2angles10thDeg, angles10thDeg2pos
+
+
+SERIAL_BAUD =  9600 #38400 #62500 #38400
 
 
 def timeName( prefix, ext, index ):
@@ -35,24 +50,62 @@ def picameraToCv2( saving = True ):
         ii = ii + 1
     return image
 
-def imageSave( workingTime = 10.0 ):
+def imageSave( workingTime = 300.0, test = 0 ):
     camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
     camera.start_preview()
     time.sleep(2)
     timeStart = time.time()
     actulalTime = time.time()
     ii = 0
-    while timeStart > actulalTime - workingTime:
+    #while timeStart > actulalTime - workingTime:
+    while True:
         actulalTime = time.time()
         index = str(ii)
         filename = timeName( "/home/pi/git/RasPI_and_robots/fire_ant/logs/image_", ".jpg", index )
         camera.capture( filename )
         ii = ii + 1
+        time.sleep(0.5)
+
+def robotGo():
+    com = LogIt( serial.Serial( '/dev/ttyAMA0', SERIAL_BAUD ) )
+    robot = FireAnt( "Due", com )
+    
+    robot.standUp()
+    ii = 0
+    while True:
+        redButton = GPIO.input(22)
+        if redButton == True:
+            break
+        
+        if ii == 3:
+            a = math.radians(0)
+            ii = 0
+        else:
+            a = 0
+            
+        robot.walk( step=(0.02, 0.0, a), numSteps=1)
+        #robot.walk( step=(0.0, -0.02, 0.0), numSteps=1) 
+        #robot.walk( step=(0.0, 0.0, math.radians(5)), numSteps=2)
+        ii += 1
+        
+    robot.sitDown()
+    robot.stopServos()
 
 
 def fireAntMain():
-    imageSave()
+    #imageSave()
     #picameraToCv2()
+    #sys.exit(1)
+    
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(22, GPIO.IN)
+    redButton = GPIO.input(22)
+    while True:
+        time.sleep(1)
+        redButton = GPIO.input(22)
+        if redButton == False:
+            robotGo()
     
 
 if __name__ == "__main__":
@@ -60,4 +113,5 @@ if __name__ == "__main__":
         print __doc__
         sys.exit()
     print "STARTED"
+    start_new_thread( imageSave, (300.0, 0) )
     fireAntMain()
