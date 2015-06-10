d007e3eae364faddadadcce8804034e06217f94a
martind <martind@mapfactor.com>
2015-06-10 20:41:38 +0200

    first attempt to integrate multiprocessing

--- fire_ant/fire_ant_main2.py
+++ fire_ant/fire_ant_main2.py
@@ -12,6 +12,7 @@ import serial
 import math
 import io
 from thread import start_new_thread
+from multiprocessing import Process, Queue
 
 sys.path.append( "/home/pi/git/fireant/serial_servo" )
 sys.path.append( "/home/pi/git/fireant/ver1")
@@ -22,6 +23,11 @@ from triangle import pos2angles10thDeg, angles10thDeg2pos
 
 
 SERIAL_BAUD =  9600 #38400 #62500 #38400
+
+g_index = 0
+g_queueOut = None
+g_processor = None
+g_queueResults = None
 
 
 def timeName( prefix, ext, index ):
@@ -50,7 +56,8 @@ def picameraToCv2( saving = True ):
         ii = ii + 1
     return image
 
-def imageSave( workingTime = 300.0, test = 0 ):
+#def imageSave( workingTime = 300.0, test = 0 ):
+def processMain( queueIn, queueOut ):
     camera = picamera.PiCamera()
     camera.resolution = (640, 480)
     camera.start_preview()
@@ -60,24 +67,54 @@ def imageSave( workingTime = 300.0, test = 0 ):
     ii = 0
     #while timeStart > actulalTime - workingTime:
     while True:
+        shouldWork = queueIn.get()
+        if shouldWork is None:
+            break
+
         actulalTime = time.time()
         index = str(ii)
         filename = timeName( "/home/pi/git/RasPI_and_robots/fire_ant/logs/image_", ".jpg", index )
         camera.capture( filename )
         ii = ii + 1
-        time.sleep(0.5)
+        queueOut.put( (filename, 1) )
+        
+#        time.sleep(0.5)
+
+def getNewPicture( firstInit ):
+    global g_queueOut, g_processor, g_queueResults
+    if g_queueOut is None:
+        g_queueOut = Queue()
+        g_queueResults = Queue()
+        g_processor = Process( target=processMain, args=(g_queueOut,g_queueResults,) )
+        g_processor.daemon = True
+        g_processor.start()
+    if firstInit:
+        g_queueOut.put_nowait( 1 )
+        return None
+    else:
+        if g_queueResults.empty():
+            return None
+        ret = g_queueResults.get()
+        g_queueOut.put_nowait( 1 )
+        return ret        
+
 
 def robotGo():
     com = LogIt( serial.Serial( '/dev/ttyAMA0', SERIAL_BAUD ) )
     robot = FireAnt( "Due", com )
     
     robot.standUp()
+    getNewPicture( firstInit=True )
     ii = 0
     while True:
         redButton = GPIO.input(22)
         if redButton == True:
             break
         
+        picResult = getNewPicture( firstInit=False )
+        if picResult is not None:
+            pass # do something
+
         if ii == 3:
             a = math.radians(0)
             ii = 0
@@ -113,5 +150,8 @@ if __name__ == "__main__":
         print __doc__
         sys.exit()
     print "STARTED"
-    start_new_thread( imageSave, (300.0, 0) )
+#    start_new_thread( imageSave, (300.0, 0) )
     fireAntMain()
+
+# vim: expandtab sw=4 ts=4 
+
